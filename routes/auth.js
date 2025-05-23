8a9054a2d5e3369b6db55e7183e8321abd3efe87
Yeom-Seung-Ah <ysa2780@naver.com>
2025-05-23 16:24:50 +0900

    Feat: 라우터 만들기

---
+++ routes/auth.js
@@ -0,0 +1,43 @@
+import express from "express";
+import User from "./../models/User.js";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+
+console.log("✅ auth.js 라우터 불러옴");
+
+const router = express.Router();
+
+// 회원가입
+router.post("/register", async (req, res) => {
+  const { username, email, password } = req.body;
+  try {
+    const hashedPw = await bcrypt.hash(password, 10);
+    const newUser = new User({ username, email, password: hashedPw });
+    await newUser.save();
+    res.status(201).json({ message: "회원가입 성공" });
+  } catch (err) {
+    res.status(500).json({ message: "회원가입 실패", error: err.message });
+  }
+});
+
+// 로그인
+router.post("/login", async (req, res) => {
+  const { email, password } = req.body;
+  try {
+    const user = await User.findOne({ email });
+    if (!user) return res.status(400).json({ message: "유저 없음" });
+
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) return res.status(401).json({ message: "비밀번호 불일치" });
+
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
+      expiresIn: "1d",
+    });
+
+    res.status(200).json({ message: "로그인 성공", token });
+  } catch (err) {
+    res.status(500).json({ message: "로그인 실패", error: err.message });
+  }
+});
+
+export default router;
