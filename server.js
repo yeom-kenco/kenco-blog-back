813e5982d90e3f364d7aaea1367eb32fd146b85c
Yeom-Seung-Ah <ysa2780@naver.com>
2025-05-26 13:24:42 +0900

    Feat: 프론트엔드 포트 번호 변경

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: "http://localhost:3000", // 나중에 프론트 포트
+    origin: "http://localhost:5173", // 나중에 프론트 포트
     credentials: true,
   })
 );
