19b60fe5d0e094a903e88b7c54e29e4ca6ce565f
Yeom-Seung-Ah <ysa2780@naver.com>
2025-05-27 14:27:21 +0900

    Feat: 회원 탈퇴 api 추가

--- routes/users/me.js
+++ routes/users/me.js
@@ -1,7 +1,7 @@
-// routes/users/me.js
 import express from "express";
 import { verifyToken } from "../../middleware/auth.js";
 import Post from "../../models/Post.js";
+import User from "../../models/User.js";
 import Comment from "../../models/Comment.js";
 
 const router = express.Router();
@@ -42,6 +42,23 @@ router.patch("/", verifyToken, async (req, res) => {
     });
   } catch (err) {
     res.status(500).json({ message: "수정 실패", error: err.message });
+  }
+});
+
+router.delete("/", verifyToken, async (req, res) => {
+  try {
+    const userId = req.userId;
+
+    // 1. 유저의 글/댓글 삭제
+    await Post.deleteMany({ author: userId });
+    await Comment.deleteMany({ author: userId });
+
+    // 2. 유저 계정 삭제
+    await User.findByIdAndDelete(userId);
+
+    res.clearCookie("token").status(200).json({ message: "회원 탈퇴 완료" });
+  } catch (err) {
+    res.status(500).json({ message: "회원 탈퇴 실패", error: err.message });
   }
 });
 
