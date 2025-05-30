a91e2d503046a6bd1eb7a51f100cd7cc5bf7aa6c
Yeom-Seung-Ah <ysa2780@naver.com>
2025-05-30 23:48:40 +0900

    Fix: 로컬에서 배포된 환경으로 변경

--- routes/uploads.js
+++ routes/uploads.js
@@ -7,8 +7,9 @@ const router = express.Router();
 // 이미지 하나 업로드
 router.post("/image", upload.single("image"), (req, res) => {
   try {
-    const filePath = `/uploads/${req.file.filename}`;
-    res.status(200).json({ url: filePath });
+    res.status(200).json({
+      url: `https://kenco-blog.onrender.com/uploads/${req.file.filename}`,
+    });
   } catch (err) {
     res.status(500).json({ message: "이미지 업로드 실패", error: err.message });
   }
